9e0ba65ad6c8eb14d2d8b3f8ce32b64c05cb1153
Fingsinz <fingsinz@outlook.com>
2024-10-05 17:51:21 +0800

    fix: 🐛 fix bug

--- src/ImgProcessor.cpp
+++ src/ImgProcessor.cpp
@@ -2,13 +2,21 @@
 #include "opencv2/imgproc.hpp"
 
 void ImgProcessor::GrayInversion(cv::Mat const &src, cv::Mat &dst) {
-    if(src.channels() == 3) {
-        cv::cvtColor(src, dst, cv::COLOR_BGR2GRAY);
-    } 
-    
-    if(src.channels() == 1) {
-        for(int i = 0; i < src.rows; ++i) {
-            for(int j = 0; j < src.cols; ++j) {
+    if (src.channels() == 3) {
+        cv::Mat tmp;
+        cv::cvtColor(src, tmp, cv::COLOR_BGR2GRAY);
+
+        for (int i = 0; i < tmp.rows; ++i) {
+            for (int j = 0; j < tmp.cols; ++j) {
+                tmp.at<uchar>(i, j) = 255 - tmp.at<uchar>(i, j);
+            }
+        }
+        dst = tmp;
+    }
+
+    if (src.channels() == 1) {
+        for (int i = 0; i < src.rows; ++i) {
+            for (int j = 0; j < src.cols; ++j) {
                 dst.at<uchar>(i, j) = 255 - src.at<uchar>(i, j);
             }
         }
